aeb2fb6c8347160a0ad06df01fb8bcdae51e894b
erjunhuang <1602220221@qq.com>
2019-12-17 18:12:08 +0800

    1.0

---
+++ Assets/RenderingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RenderingObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        RenderingMaster.RegisterObject(this);
+    }
+
+    // Update is called once per frame
+    void OnDisable()
+    {
+        RenderingMaster.UnregisterObject(this);
+    }
+}
